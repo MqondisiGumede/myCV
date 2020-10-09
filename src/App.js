4577f0c55af8cd96d8e900863576eae5483b1638
MqondisiGumede <mqondi.gumede@gmail.com>
2020-10-10 01:28:34 +0200

    Structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'materialize-css/dist/css/materialize.min.css';
+import 'materialize-css/dist/js/materialize.min.js';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './Components/Home';
+import Skill from './Components/Skill';
+import Experience from './Components/Experience';
+import Education from './Components/Education';
+import Portfolio from './Components/Portfolio';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router> 
+      <>
+        <Route exact path="/" component={Home}/>
+        <Switch>
+        < Route path='/skill' component={Skills}/>
+        < Route path='/education' component={Education} />
+        < Route path='/experience' component={Experience} />
+        < Route path='/portfolio' component={Portfolio} />
+
+        </Switch>
+      </>
+    </Router>
   );
 }
 
